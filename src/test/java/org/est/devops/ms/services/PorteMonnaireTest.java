df28b830b7f089fce265e14c1e8ae36a0c782daa
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2025-04-24 16:06:59 +0100

    Trigger GitHub Actions for main branch

--- src/test/java/org/est/devops/ms/services/PorteMonnaireTest.java
+++ src/test/java/org/est/devops/ms/services/PorteMonnaireTest.java
@@ -10,7 +10,7 @@ class PorteMonnaieTest {
         pm.ajouterSomme(new SommeArgent(10, "EUR"));
         pm.ajouterSomme(new SommeArgent(15, "EUR"));
         pm.ajouterSomme(new SommeArgent(5, "USD"));
-        assertEquals(2, pm.montantTotalPourDevise("EUR"));
+        assertEquals(25, pm.montantTotalPourDevise("EUR"));
         assertEquals(5, pm.montantTotalPourDevise("USD"));
         assertEquals(0, pm.montantTotalPourDevise("GBP"));
     }
